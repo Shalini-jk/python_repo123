cd1c900eb6c6cd9a66c27d34e5d6f20f614a45ad
Shalini-jk <shalini@ambersoft.in>
2023-03-17 16:24:23 +0530

    Program to find greater value and divide it by smaller value

---
+++ Programs/greater.py
@@ -0,0 +1,12 @@
+a=int(input("enter the first no:\n"))
+b=int(input("enter the second no:\n"))
+if(a>b):
+    print("a is greater among them")
+    c=a/b;
+    print(c)
+else:
+    print("b is greater among them")
+    c=b/a;
+    print(c)
+     
+    
