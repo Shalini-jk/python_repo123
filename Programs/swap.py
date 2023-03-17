713238be7a119c55fd5a703f0d906e725e48e40f
Shalini-jk <shalini@ambersoft.in>
2023-03-17 16:35:16 +0530

    Swapping Two numbers

---
+++ Programs/swap.py
@@ -0,0 +1,9 @@
+a = (int(input("enter a number - ")))
+b = (int(input("enter a number - ")))
+print("numbers before swaping - ")
+print(a,b)
+
+a,b=b,a
+print("numbers after swaping - ")
+print(a,b)
+
