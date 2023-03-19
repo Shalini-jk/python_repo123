b03407435e70a3ae074931dad2f0a27ea8696cd8
Shalini-jk <shalini@ambersoft.in>
2023-03-19 15:10:27 +0530

    To find whether the number is positive or negative

---
+++ Programs2/Positive.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a number \n"))
+if(a>0):
+    print("the given number is positive")
+elif(a<0):
+    print("the given number is negative")
+elif(a==0):
+    print("the given number is 0")        
+    
+     
+    
+    
