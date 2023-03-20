3edae0aa5bc2e59e2e3a03986dd86d1da697588b
Shalini-jk <shalini@ambersoft.in>
2023-03-20 15:34:28 +0530

    Program to find simple interest

---
+++ Programs2/SimpleInterest.py
@@ -0,0 +1,5 @@
+p = int (input("Enter the principle amount \n"))
+r = int ( input("enter the rate for the principle \n"))
+t = int (input(" enter the time period \n"))
+si = (p*r*t)/100;
+print("the simple interest :",si)
