131a45c354dc926772b4f9390eb21ffa13e5bf9a
Shalini-jk <shalini@ambersoft.in>
2023-03-22 11:42:58 +0530

    Program to Concatenate two String in Single String

---
+++ String_Programs/Concat_String.py
@@ -0,0 +1,7 @@
+First_String = input("Enter the first String: \n ")
+Second_String = input("Enter the Second String: \n")
+New_String = First_String + Second_String # by adding two string value
+print (New_String)
+
+New_String1 = "   ".join([First_String , Second_String]) # by using join()
+print(New_String1)
