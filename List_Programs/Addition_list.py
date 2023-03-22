bb8444a714de8ebfd85c2acc2a33ad775d6a689f
Shalini-jk <shalini@ambersoft.in>
2023-03-22 13:21:58 +0530

    Prgram to add the value of two item

--- List_Programs/Addition_list.py
+++ List_Programs/Addition_list.py
@@ -1,10 +1,10 @@
 list1 = [2,4,6,8]
 list2 = [3,8,7,5,10,12]
 list3 = []
-list3.append(list1 + list2) 
+list3.append(list1 + list2)  # it only add the value of two list in one
 print (list3)
 
-
+# here we can add each element of first list to second list and the new list store the resultant
 result = [] 
 small_list = len(list1) < len(list2) and list1 or list2
 
