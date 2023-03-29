e0a62e1bd9fb033f5264f7c0e4f206f6934f3302
Shalini-jk <shalini@ambersoft.in>
2023-03-29 10:26:20 +0530

    programs on various data structure

---
+++ DataStructure/Queue.py
@@ -0,0 +1,18 @@
+# queue works on first in first out principle(fifo)
+
+queue = []
+
+queue.append(3)
+queue.append(4)
+queue.append(5)
+queue.append(6)
+queue.append(7)
+queue.append(8)
+queue.append(9)
+ 
+print(queue)
+ 
+print(queue.pop(0))
+print(queue.pop(0))
+ 
+print(queue)
