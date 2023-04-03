6c65f9783821e12603ebf041868c8956f476e92b
Shalini-jk <127472079+Shalini-jk@users.noreply.github.com>
2023-04-02 22:27:10 -0700

    Updated Program of implementation of data Structure using python (#17)

--- DataStructure/Stack.py
+++ DataStructure/Stack.py
@@ -1,23 +1,45 @@
 # stack works on first in last out number (filo) or last in first out principle (lifo)
  
-stack_example = []
- 
-stack_example.append('one')
-stack_example.append('two')
-stack_example.append('three')
-stack_example.append('four')
- 
-print('Initial stack with 4 elements one, two, three, four loaded in sequence')
-print(stack_example)
- 
-print('3 Elements popped from stack in LIFO order:')
-print(stack_example.pop())
-print(stack_example.pop())
-print(stack_example.pop())
- 
-print('\nStack after 3 elements are popped:')
-print(stack_example)
+class Stack:
+     def __init__ (self):
+         self. items = []
 
-print('\nStack is left with one element only')
-print(stack_example.pop())
+     def isEmpty (self):
+         return self. items == []
 
+     def push (self, item):
+         self. items.append (item)
+
+     def pop (self):
+         return self. items.pop()
+
+     def peek (self):
+         return self. items [len(self.items)-1]
+
+     def size (self):
+         return len (self.items)
+     
+s = Stack()
+
+print (s. isEmpty())
+s.push ("Student")
+s.push ("Teacher")
+s.push ("Docter")
+s.push ("Lawyer")
+s.push ("Engineer")
+s.push ("Scientist")
+
+print ("getting top element after inserting some elements in the stack:   ", s.peek())
+
+s.push ("Judge")
+
+print ( "Size of the Stack after entering all the element :  ", s.size())
+print (" Here we check that still the stack is empty or not after entering all the elements :  ",s.isEmpty())
+print ("After pushing one new  element we find the  Peek(top) element :   ", s.peek())
+
+s.push ("Advocate")
+
+print (s.pop())
+print (s.pop())
+print (" After pop (deleting some items from stack we again get the Top element:  )",s.peek())
+print("Total no of items present in stack after performing all the operation:  ", s.size())
