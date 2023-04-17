a5d7694aae3fde6cd9fac87908326c6be6e92bed
Shalini-jk <Shalini@ambersoft.in>
2023-04-17 10:50:02 +0530

    oops concept

---
+++ OopsConcept/Circles.py
@@ -0,0 +1,18 @@
+a = int(input("Enter the  radius of circle :  "))
+
+
+class Circles:
+    
+  def area():
+      arearec = 4 * 3.14 * a
+      print("The area  of Circle is :  ", arearec)
+      
+  def perimeter():
+      peri =  2 * 3.14 * a
+      print(" The perimeter of rectangle is : ", peri)
+        
+
+    
+      
+obj = Circles
+obj.area()
